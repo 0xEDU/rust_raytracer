31c3ab4ee7a91849c88405777a60970e6e69d8f9
0xEDU <bfwaend@gmail.com>
2023-06-11 14:39:23 -0300

    tests: add point/vector creation tests

--- tests/tuple_tests.rs
+++ tests/tuple_tests.rs
@@ -1,16 +1,34 @@
-use rt_challenge::tuple::Tuple;
+use rt_challenge::tuple::*;
 
 #[test]
-fn simple_tuples_test() {
-    let a: Tuple = Tuple {x: 1.0, y: 1.0, z: 1.0, w: 0.0 };
-    assert_eq!(a.x, 1.0);
-    assert_eq!(a.y, 1.0);
-    assert_eq!(a.z, 1.0);
-    assert_eq!(a.is_vector(), true);
+fn tuples_instatiation_test() {
+    let v: Tuple = Tuple {x: 1.0, y: 1.0, z: 1.0, w: 0.0 };
+    assert_eq!(v.x, 1.0);
+    assert_eq!(v.y, 1.0);
+    assert_eq!(v.z, 1.0);
+    assert_eq!(v.is_vector(), true);
 
-    let a: Tuple = Tuple {x: 1.0, y: 1.0, z: 1.0, w: 1.0 };
-    assert_eq!(a.x, 1.0);
-    assert_eq!(a.y, 1.0);
-    assert_eq!(a.z, 1.0);
-    assert_eq!(a.is_direction(), true);
+    let d: Tuple = Tuple {x: 1.0, y: 1.0, z: 1.0, w: 1.0 };
+    assert_eq!(d.x, 1.0);
+    assert_eq!(d.y, 1.0);
+    assert_eq!(d.z, 1.0);
+    assert_eq!(d.is_point(), true);
+}
+
+#[test]
+fn point_creation_test() {
+    let a: Point = point(4.0, -4.0, 3.0);
+    assert_eq!(a.is_point(), true);
+    assert_eq!(a.x, 4.0);
+    assert_eq!(a.y, -4.0);
+    assert_eq!(a.z, 3.0);
+}
+
+#[test]
+fn vector_creation_test() {
+    let a: Vector = vector(4.0, -4.0, 3.0);
+    assert_eq!(a.is_vector(), true);
+    assert_eq!(a.x, 4.0);
+    assert_eq!(a.y, -4.0);
+    assert_eq!(a.z, 3.0);
 }
