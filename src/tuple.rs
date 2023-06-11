8691474d8fbc42c71c7c307f6b1293388874774f
0xEDU <bfwaend@gmail.com>
2023-06-11 14:39:45 -0300

    feat(Tuple): add point/vector instatiation functions

--- src/tuple.rs
+++ src/tuple.rs
@@ -5,14 +5,25 @@ pub struct Tuple {
     pub w: f64,
 }
 
+pub type Point = Tuple;
+pub type Vector = Tuple;
+
 impl Tuple {
     pub fn is_vector(&self) -> bool {
         let result = if self.w == 0.0 {true} else {false};
         result
     }
 
-    pub fn is_direction(&self) -> bool {
+    pub fn is_point(&self) -> bool {
         let result = if self.w == 1.0 {true} else {false};
         result
     }
+}
+
+pub fn point(x: f64, y: f64, z: f64) -> Point {
+    Point { x, y, z, w: 1.0 }
+}
+
+pub fn vector(x: f64, y: f64, z: f64) -> Vector {
+    Vector { x, y, z, w: 0.0 }
 }
