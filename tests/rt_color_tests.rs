3f799108a2ad291a254cf1ec37dd85b776a028b1
etachott <bfwaend@gmail.com>
2023-06-12 17:05:58 -0300

    tests(RTColor): rename RTColor test function

--- tests/rt_color_tests.rs
+++ tests/rt_color_tests.rs
@@ -1,14 +1,14 @@
 use rt_challenge::rt_color::*;
 
 #[test]
-fn color_creation_test() {
+fn rt_color_creation_test() {
     let c: RTColor = color(-0.5, 0.4, 1.7);
     let result: RTColor = RTColor {r: -0.5, g: 0.4, b: 1.7};
     assert!(c == result);
 }
 
 #[test]
-fn tuple_operator_overload_test() {
+fn rt_color_operator_overload_test() {
     let a: RTColor = color(1.0, 1.0, 1.0);
     let b: RTColor = color(1.0, 1.0, 1.0);
     let result: RTColor = color(2.0, 2.0, 2.0);
