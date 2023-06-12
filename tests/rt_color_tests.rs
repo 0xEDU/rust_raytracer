f0a5b58bf2dee8cafb0a18d3959340ffafb2b5a8
0xEDU <bfwaend@gmail.com>
2023-06-12 01:11:23 -0300

    tests(RTColor): add overloads tests

--- tests/rt_color_tests.rs
+++ tests/rt_color_tests.rs
@@ -6,3 +6,22 @@ fn color_creation_test() {
     let result: RTColor = RTColor {r: -0.5, g: 0.4, b: 1.7};
     assert!(c == result);
 }
+
+#[test]
+fn tuple_operator_overload_test() {
+    let a: RTColor = color(1.0, 1.0, 1.0);
+    let b: RTColor = color(1.0, 1.0, 1.0);
+    let result: RTColor = color(2.0, 2.0, 2.0);
+    assert!((a + b) == result);
+
+    let a: RTColor = color(1.0, 1.0, 1.0);
+    let b: RTColor = color(1.0, 1.0, 1.0);
+    let result: RTColor = color(0.0, 0.0, 0.0);
+    assert!((a - b) == result);
+
+    let a: RTColor = color(2.0, 2.0, 2.0);
+    let b: RTColor = color(2.0, 2.0, 2.0);
+    let result: RTColor = RTColor {r: 4.0, g: 4.0, b: 4.0};
+    assert!(a * b == result);
+
+}
