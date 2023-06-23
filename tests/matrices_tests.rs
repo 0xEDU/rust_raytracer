db8b20f5ccf115c609c65b3ac37e2188522fefb6
0xEDU <bfwaend@gmail.com>
2023-06-23 00:02:45 -0300

    tests(Matrices): add test for Matrix struct instantiation and equality overload

--- tests/matrices_tests.rs
+++ tests/matrices_tests.rs
@@ -2,12 +2,14 @@ use rt_challenge::matrices::*;
 
 #[test]
 fn matrix_instatiation_test() {
-    let v = vec![
-        1.0, 1.0, 1.0, 1.0,
-        1.0, 1.0, 1.0, 1.0,
-        1.0, 1.0, 1.0, 1.0,
-        1.0, 1.0, 1.0, 1.0,
-    ];
-    let m = Matrix::new(4, 4, v);
-    assert!(m.at(0, 0) == 1.0);
+    let mut m: Matrix = Matrix::new();
+    for i in 0..3 {
+        for j in 0..3 {
+            m.data[i][j] = (i + j) as f64;
+            assert!(m.data[i][j] == (i + j) as f64);
+        }
+    }
+    let m: Matrix = Matrix::new();
+    let n: Matrix = Matrix::new();
+    assert!(m == n);
 }
