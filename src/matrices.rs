75d196cf4e64e450aafa2b2d68901dcd75a0458d
0xEDU <bfwaend@gmail.com>
2023-06-24 03:31:08 -0300

    feat(Matrices): add determinant, sub_matrix and cofactor (broken) methods

--- src/matrices.rs
+++ src/matrices.rs
@@ -33,6 +33,50 @@ impl Matrix {
         }
         t
     }
+
+    pub fn determinant(&self) -> f64 {
+        let sub_det_0 = self.data[2][2] * self.data[3][3] - self.data[2][3] * self.data[3][2];
+        let sub_det_1 = self.data[2][1] * self.data[3][3] - self.data[2][3] * self.data[3][1];
+        let sub_det_2 = self.data[2][1] * self.data[3][2] - self.data[2][2] * self.data[3][1];
+        let sub_det_3 = self.data[2][0] * self.data[3][3] - self.data[2][3] * self.data[3][0];
+        let sub_det_4 = self.data[2][0] * self.data[3][2] - self.data[2][2] * self.data[3][0];
+        let sub_det_5 = self.data[2][0] * self.data[3][1] - self.data[2][1] * self.data[3][0];
+
+        self.data[0][0] * (self.data[1][1] * sub_det_0 - self.data[1][2] * sub_det_1 + self.data[1][3] * sub_det_2) -
+        self.data[0][1] * (self.data[1][0] * sub_det_0 - self.data[1][2] * sub_det_3 + self.data[1][3] * sub_det_4) +
+        self.data[0][2] * (self.data[1][0] * sub_det_1 - self.data[1][1] * sub_det_3 + self.data[1][3] * sub_det_5) -
+        self.data[0][3] * (self.data[1][0] * sub_det_2 - self.data[1][1] * sub_det_4 + self.data[1][2] * sub_det_5)
+    }
+
+    pub fn cofactor(&self, row: usize, col: usize) -> f64 {
+        let sub_matrix = self.submatrix(row, col);
+        let sign = if (row + col) % 2 == 0 { 1.0 } else { -1.0 };
+        // println!("{}", sub_matrix);
+        sign * sub_matrix.determinant()
+    }
+
+    pub fn submatrix(&self, row: usize, col: usize) -> Matrix {
+        let mut sub_matrix = Matrix::new();
+
+        let mut sub_row = 0;
+        for i in 0..4 {
+            if i == row {
+                continue;
+            }
+
+            let mut sub_col = 0;
+            for j in 0..4 {
+                if j == col {
+                    continue;
+                }
+                sub_matrix.data[sub_row][sub_col] = self.data[i][j];
+                println!("{} {} {}", self.data[i][j], i, j);
+                sub_col += 1;
+            }
+            sub_row += 1;
+        }
+        sub_matrix
+    }
 }
 /* ========================================================================== */
 
