e5ca90e55a9884f1db29bcf6989e95eeaf00dbbc
0xEDU <bfwaend@gmail.com>
2023-07-28 01:30:08 -0300

    feat(Matrix transformations): add rotation over the x axis

--- src/matrix_transformations.rs
+++ src/matrix_transformations.rs
@@ -15,3 +15,12 @@ pub fn scaling(x: f64, y: f64, z: f64) -> Matrix {
     m.data[2][2] = z;
     m
 }
+
+pub fn rotation_x(r: f64) -> Matrix {
+    let mut m: Matrix = Matrix::identity();
+    m.data[1][1] = r.cos();
+    m.data[1][2] = -(r.sin());
+    m.data[2][1] = r.sin();
+    m.data[2][2] = -(r.cos());
+    m
+}
