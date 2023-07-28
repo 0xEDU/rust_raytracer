3d1713f62e05b09297bd9dad472e0bfc3e6e1252
0xEDU <bfwaend@gmail.com>
2023-07-28 00:48:36 -0300

    tests(Matrix transformations): add tests for scaling

--- tests/matrix_transformations_tests.rs
+++ tests/matrix_transformations_tests.rs
@@ -21,3 +21,25 @@ fn vector_translation_test() {
     let v: Tuple = vector(-3.0, 4.0, 5.0);
     assert!(transform * v == v);
 }
+
+#[test]
+fn point_scaling_test() {
+    let transform = scaling(2.0, 3.0, 4.0);
+    let p = point(-4.0, 6.0, 8.0);
+    assert!(transform * p == point(-8.0, 18.0, 32.0));
+}
+
+#[test]
+fn vector_scaling_test() {
+    let transform = scaling(2.0, 3.0, 4.0);
+    let v = vector(-4.0, 6.0, 8.0);
+    assert!(transform * v == vector(-8.0, 18.0, 32.0));
+}
+
+#[test]
+fn inverse_scaling_test() {
+    let transform = scaling(2.0, 3.0, 4.0);
+    let inv = transform.inverse().unwrap();
+    let v = vector(-4.0, 6.0, 8.0);
+    assert!(inv * v == vector(-2.0, 2.0, 2.0));
+}
