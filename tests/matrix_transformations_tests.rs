54b78516c24b81cb4b0b550ea87a65353152164d
0xEDU <bfwaend@gmail.com>
2023-07-29 00:53:30 -0300

    tests(Matrix transformation): add test for chaining transformations

--- tests/matrix_transformations_tests.rs
+++ tests/matrix_transformations_tests.rs
@@ -4,6 +4,13 @@ use std::f64::consts::SQRT_2;
 use float_cmp::approx_eq;
 use rt_challenge::{matrix_transformations::*, tuple::{Tuple, point, vector}};
 
+fn compare_tuples(result: Tuple, expected: Tuple) -> bool {
+    let x = approx_eq!(f64, result.x, expected.x, (0.03, 2));
+    let y = approx_eq!(f64, result.y, expected.y, (0.03, 2));
+    let z = approx_eq!(f64, result.z, expected.z, (0.03, 2));
+    x && y && z
+}
+
 #[test]
 fn simple_translation_test() {
     let transform = translation(5.0, -3.0, 2.0);
@@ -58,15 +65,10 @@ fn x_axis_rotation_test() {
     let half_expected = point(0.0, SQRT_2/2.0, SQRT_2/2.0);
 
     let full_result = full_quarter * p;
-    let full_expected = point(0.0, 0.0, 0.0);
+    let full_expected = point(0.0, 0.0, 1.0);
 
-    approx_eq!(f64, half_result.x, half_expected.x, ulps = 15);
-    approx_eq!(f64, half_result.y, half_expected.x, ulps = 15);
-    approx_eq!(f64, half_result.z, half_expected.x, ulps = 15);
-
-    approx_eq!(f64, full_result.x, full_expected.x, ulps = 15);
-    approx_eq!(f64, full_result.y, full_expected.x, ulps = 15);
-    approx_eq!(f64, full_result.z, full_expected.x, ulps = 15);
+    compare_tuples(half_result, half_expected);
+    compare_tuples(full_result, full_expected);
 }
 
 #[test]
@@ -164,4 +166,21 @@ fn z_to_y_shearing_transformation_test() {
     let transform = shearing(0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
     let p = point(2.0, 3.0, 4.0);
     assert!(transform * p == point(2.0, 3.0, 7.0));
+}
+
+#[test]
+fn chaining_transformations_test() {
+    let p = point(1.0, 0.0, 1.0);
+    let a = rotation_x(PI / 2.0);
+    let b = scaling(5.0, 5.0, 5.0);
+    let c = translation(10.0, 5.0, 7.0);
+
+    let p2 = a * p;
+    assert!(compare_tuples(p2, point(1.0, -1.0, 0.0)));
+
+    let p3 = b * p2;
+    assert!(compare_tuples(p3, point(5.0, -5.0, 0.0)));
+
+    let p4 = c * p3;
+    assert!(compare_tuples(p4, point(15.0, 0.0, 7.0)));
 }
