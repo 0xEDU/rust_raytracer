7f6495c4151b6793f9b339453a9e6c7928beea9b
0xEDU <bfwaend@gmail.com>
2023-07-29 00:03:30 -0300

    feat(Matrix transformations): add shearing transformation

--- src/matrix_transformations.rs
+++ src/matrix_transformations.rs
@@ -16,6 +16,19 @@ pub fn scaling(x: f64, y: f64, z: f64) -> Matrix {
     m
 }
 
+pub fn
+shearing(xy: f64, xz: f64, yx: f64, yz: f64, zx: f64, zy: f64) -> Matrix {
+    let mut m: Matrix = Matrix::identity();
+    m.data[0][1] = xy;
+    m.data[0][2] = xz;
+    m.data[1][0] = yx;
+    m.data[1][2] = yz;
+    m.data[2][0] = zx;
+    m.data[2][1] = zy;
+    m
+}
+
+/* Rotations ---------------------------------------------------------------- */
 pub fn rotation_x(r: f64) -> Matrix {
     let mut m: Matrix = Matrix::identity();
     m.data[1][1] = r.cos();
@@ -42,3 +55,4 @@ pub fn rotation_z(r: f64) -> Matrix {
     m.data[1][1] = r.cos();
     m
 }
+/* -------------------------------------------------------------------------- */
